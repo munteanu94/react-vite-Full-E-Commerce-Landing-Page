2592d2c4830bc84396727310de4784e18f4abfef
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2024-03-01 22:48:59 +0200

    some changes for deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import FirstBlock from "../component/homeComponent/firstContainer/FirstBlock";
-import TabList from "../component/homeComponent/secondContainer/TabList";
-import PopularProducts from "../component/homeComponent/FirdContainer/PopularProducts";
+
 export default function Home() {
   return (
     <>
-      <FirstBlock />
-      <TabList />
-      <PopularProducts />
+      <h1>hello Home</h1>
     </>
   );
 }
